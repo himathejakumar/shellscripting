3f23c599f9d2b6106e1fa59acf6cca857345d260
himathejakumar <33077339+himathejakumar@users.noreply.github.com>
2018-08-19 20:11:39 +0530

    Update basicsalary.sh

--- basicsalary.sh
+++ basicsalary.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 echo "Enter basic salary"
 bs=$1
-
 dp=`expr $bs \* 50`
 dap=`expr $dp / 100`
 bdap=`expr $dap + $bs`
-
 da=`expr $bdap \* 35`
 daa=`expr $da / 100`
-
 hra=`expr $bdap \* 8`
 hraa=`expr $hra / 100`
-
 ma=`expr $bdap \* 3`
 maa=`expr $ma / 100`
-
 pf=`expr $bdap \* 10`
 paf=`expr $pf / 100`
-
 salary=`expr $bs + $dap + $daa + $hraa + $maa`
 netsalary=`expr $salary - $paf`
-
-
 echo "salary=$netsalary"
